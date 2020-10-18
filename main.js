2235445fc27f805e22dc9fdf7c1795903c0a908f
Arthur Arend <arthur.nox@gmail.com>
2020-10-18 02:49:11 -0300

    Nav

--- main.js
+++ main.js
@@ -26,4 +26,37 @@ OPEN_BUTTON.onclick = function(){
 const PREV = document.querySelector('.previus')
 const NEXT = document.querySelector('.next')
 
+const ONE = document.querySelector('.slide-one')
+const TWO = document.querySelector('.slide-two')
+const THREE = document.querySelector('.slide-three')
+
+let index = 0
+
+function plusSlides(n) {
+  index = index + n
+
+  if(index < 0){
+    console.log('zero')
+    index = 3
+  } else if(index > 3){
+    console.log('quattro')
+    index = 1
+  }
+  console.log(index)
+
+  if(index == 1){
+    ONE.style.display = 'flex'
+    TWO.style.display = 'none'
+    THREE.style.display = 'none'
+  } else if (index == 2) {
+    ONE.style.display = 'none'
+    TWO.style.display = 'flex'
+    THREE.style.display = 'none'
+  } else if (index == 3) {
+    ONE.style.display = 'none'
+    TWO.style.display = 'none'
+    THREE.style.display = 'flex'
+  }
+
+}
 
